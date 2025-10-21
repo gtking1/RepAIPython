1963dc56f5ae1a7bc370e3b35c80516936624983
Grant King <gtking@email.sc.edu>
2025-10-21 19:23:50 -0400

    Mansur recorded

--- read.py
+++ read.py
@@ -3,9 +3,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 plt.rcParams['figure.figsize'] = 40, 12
 
-data = np.loadtxt('10192025547PMData', delimiter=',')
-setMarkers = np.loadtxt('10192025547PMSetMarkers', delimiter='\0')
-repMarkers = np.loadtxt('10192025547PMRepMarkers', delimiter='\0')
+data = np.loadtxt('10202025636PMData', delimiter=',')
+setMarkers = np.loadtxt('10202025636PMSetMarkers', delimiter='\0')
+repMarkers = np.loadtxt('10202025636PMRepMarkers', delimiter='\0')
 print(len(data))
 # print(len(setMarkers))
 #data = data.reshape((len(data) // 8, 8))
@@ -14,10 +14,10 @@ df = pd.DataFrame(data[0:,0:])
 print(len(df))
 df.columns = ['Time', 'AccX', 'AccY', 'AccZ', 'GyrX', 'GyrY', 'GyrZ', 'HR']
 df['Exercise'] = pd.Series(['Idle'] * len(df))
-df.loc[(df.Time >= 11.449658989906311) & (df.Time <= 74.87261891365051), "Exercise"] = 'BarbellSquat'
-df.loc[(df.Time >= 446.7501759529114) & (df.Time <= 494.95501494407654), "Exercise"] = 'BarbellSquat'
-df.loc[(df.Time >= 867.3062160015106) & (df.Time <= 918.1417340040207), "Exercise"] = 'BarbellSquat'
-df.loc[(df.Time >= 1584.1057080030441) & (df.Time <= 1624.5065599679947), "Exercise"] = 'SmithMachineCalfRaise'
+df.loc[(df.Time >= 237.60957300662994) & (df.Time <= 279.49456095695496), "Exercise"] = 'MachineChestPress'
+df.loc[(df.Time >= 637.5524300336838) & (df.Time <= 680.0952860116959), "Exercise"] = 'MachineChestPress'
+df.loc[(df.Time >= 943.0562419891357) & (df.Time <= 988.0508450269699), "Exercise"] = 'RopeOverheadTricepsExtension'
+df.loc[(df.Time >= 1171.8675060272217) & (df.Time <= 1214.2162539958954), "Exercise"] = 'DumbbellLateralRaise'
 # df['Exercise'] = pd.Series()
 print(df.head())
 print(df.tail())
